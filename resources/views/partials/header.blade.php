9b5e4cfefe8d3f12fea7f006a49f00d4d80256aa
Michele Curtaz <mcurtaz@gmail.com>
2020-09-18 16:04:38 +0200

    list header link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,10 @@
         <img src="{{ asset("img/marchio-sito-test.png") }}" alt="logo">
         </div>
         <nav class="auto-margin">
-        <a href=" {{ route("home") }}">home</a>
-        <a href=" {{ route("home") }}">prodotti</a>
-        <a href=" {{ route("home") }}">news</a>
+            <ul>
+                <li><a href=" {{ route("home") }}">home</a></li>
+                <li><a href=" {{ route("home") }}">prodotti</a></li>
+                <li><a href=" {{ route("home") }}">news</a></li>
+            </ul>
         </nav>
 </header>
