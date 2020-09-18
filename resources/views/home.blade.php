a55274b1388d853630bbb749d03962395db2433f
Michele Curtaz <mcurtaz@gmail.com>
2020-09-18 15:59:21 +0200

    showcase

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,42 @@
-@php
-    var_dump($lunghe);
-@endphp
+@extends('layouts.homelayout')
+
+@section('pastas')
+
+    <div id="cards-container" class="auto-margin">
+
+        <h2>Le Lunghe</h2>
+        <div class="showcase">
+
+            @foreach ($lunghe as $pasta)
+                <div class="card">
+                    <img src="{{ $pasta["src"] }}" alt="img">
+                </div>
+            @endforeach
+
+        </div>
+
+        <h2>Le Corte</h2>
+        <div class="showcase">
+
+            @foreach ($corte as $pasta)
+                <div class="card">
+                    <img src="{{ $pasta["src"] }}" alt="img">
+                </div>
+            @endforeach
+
+        </div>
+
+        <h2>Le Cortissime</h2>
+        <div class="showcase">
+
+            @foreach ($cortissime as $pasta)
+                <div class="card">
+                    <img src="{{ $pasta["src"] }}" alt="img">
+                </div>
+            @endforeach
+
+        </div>
+
+
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="logo-container">
+        <img src="{{ asset("img/marchio-sito-test.png") }}" alt="logo">
+        </div>
+        <nav class="auto-margin">
+        <a href=" {{ route("home") }}">home</a>
+        <a href=" {{ route("home") }}">prodotti</a>
+        <a href=" {{ route("home") }}">news</a>
+        </nav>
+</header>
